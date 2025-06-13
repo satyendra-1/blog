488992db5d77199c9ad09c116134a22b1245078c
satyendra-1 <srisatya5314@gmail.com>
2025-06-13 21:09:40 +0530

    fixing server.js and vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import 'dotenv/config';
-import cors from 'cors';//allow backend to any frontend url
+import cors from 'cors';
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
 
-const app=express();
+const app = express();
 await connectDB(); 
-//Middlewares
-app.use(cors())
-app.use(express.json())
-//Routes
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
-app.use('/api/admin',adminRouter);
-app.use('/api/blog',blogRouter);
-const PORT=process.env.PORT||3000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
-export default app; 
+app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
+
+//  No app.listen() for Vercel
+export default app;
